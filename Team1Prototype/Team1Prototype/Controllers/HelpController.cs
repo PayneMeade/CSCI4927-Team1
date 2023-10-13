612e35fa9cf4e465c7dcc0fcfb9fc4306ea4895e
thomas <thomas.foreman17@gmail.com>
2023-10-13 19:06:12 -0400

    Added the help page link on layout.cshtml and help page with figma css.

---
+++ Team1Prototype/Team1Prototype/Controllers/HelpController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Team1Prototype.Models;
+
+namespace Team1Prototype.Controllers
+{
+    public class HelpController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(HelpRequest request)
+        {
+            if (ModelState.IsValid)
+            {
+                // Here, you can handle the submitted data as you wish
+                // For example, send an email, log it, etc.
+                // Since we're not saving to a database, just as an example:
+                ViewBag.Message = "Help request submitted successfully!";
+                return View();
+            }
+            return View(request);
+        }
+    }
+}
---
+++ Team1Prototype/Team1Prototype/Models/HelpRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace Team1Prototype.Models
+{
+    public class HelpRequest
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Message { get; set; }
+    }
+}
