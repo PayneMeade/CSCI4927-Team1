5268380d41b8e7df09c0f9e5483d83c814fe7464
Andrew McDonald <mcdonaldai@etsu.edu>
2023-11-07 15:14:02 -0500

    Adds changes to help page and _Layout

--- Team1Prototype/Team1Prototype/wwwroot/js/site.js
+++ Team1Prototype/Team1Prototype/wwwroot/js/site.js
@@ -1,15 +1,21 @@
 ﻿/* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
 function openCourseSidebar() {
+    document.getElementById("mainContent").style.opacity = "0";
+
     document.getElementById("instructorSidebar").style.visibility = "hidden";
     document.getElementById("courseSidebar").style.visibility = "visible";
+
     document.getElementById("courseSidebar").style.width = "250px";
     document.getElementById("instructorSidebar").style.width = "0px"
     document.getElementById("main").style.marginLeft = "250px";
 }
 
 function openInstructorSidebar() {
+    document.getElementById("mainContent").style.opacity = "0";
+
     document.getElementById("instructorSidebar").style.visibility = "visible";
     document.getElementById("courseSidebar").style.visibility = "hidden";
+
     document.getElementById("instructorSidebar").style.width = "250px";
     document.getElementById("courseSidebar").style.width = "0px";
     document.getElementById("main").style.marginLeft = "250px";
@@ -17,11 +23,15 @@ function openInstructorSidebar() {
 
 /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
 function closeCourseSidebar() {
+    document.getElementById("mainContent").style.opacity = "1";
+
     document.getElementById("courseSidebar").style.width = "0px";
     document.getElementById("main").style.marginLeft = "0px";
 }
 
 function closeInstructorSidebar() {
+    document.getElementById("mainContent").style.opacity = "1";
+
     document.getElementById("instructorSidebar").style.width = "0px";
     document.getElementById("main").style.marginLeft = "0px";
 }
