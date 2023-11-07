d94a53db16d501eeca5c46150dbe88b973512a04
Andrew McDonald <mcdonaldai@etsu.edu>
2023-11-07 14:47:00 -0500

    Changes css, js, and _Layout/Index pages to make site cleaner

--- Team1Prototype/Team1Prototype/wwwroot/js/site.js
+++ Team1Prototype/Team1Prototype/wwwroot/js/site.js
@@ -1,11 +1,15 @@
 ﻿/* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
 function openCourseSidebar() {
+    document.getElementById("instructorSidebar").style.visibility = "hidden";
+    document.getElementById("courseSidebar").style.visibility = "visible";
     document.getElementById("courseSidebar").style.width = "250px";
     document.getElementById("instructorSidebar").style.width = "0px"
     document.getElementById("main").style.marginLeft = "250px";
 }
 
 function openInstructorSidebar() {
+    document.getElementById("instructorSidebar").style.visibility = "visible";
+    document.getElementById("courseSidebar").style.visibility = "hidden";
     document.getElementById("instructorSidebar").style.width = "250px";
     document.getElementById("courseSidebar").style.width = "0px";
     document.getElementById("main").style.marginLeft = "250px";
